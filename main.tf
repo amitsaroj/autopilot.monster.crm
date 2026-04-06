58dca1bbba966a2c436fa72fda737e2f4a822949
Amit Saroj <sarojamit4956@gmail.com>
2026-04-07 00:49:30 +0530

    Updated main.tf with the complete Terraform configuration for autopilot resources.

--- main.tf
+++ main.tf
@@ -1,34 +1,34 @@
-# Terraform Configuration for AWS
-
-## IAM Roles
-resource "aws_iam_role" "example_role" {
-  name               = "example_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-      Effect   = "Allow"
-      Sid      = ""
-    }]
-  })
+resource "aws_iam_role" "autopilot_ssm_role" {
+  name = "autopilot-ssm-role"
+  assume_role_policy = jsonencode(
+    {
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = "sts:AssumeRole"
+          Principal = { Service = "ssm.amazonaws.com" }
+          Effect = "Allow"
+          Sid = ""
+        }
+      ]
+    }
+  )
 }
 
-## Security Groups
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH from anywhere"
-  vpc_id      = aws_vpc.example_vpc.id
-
+resource "aws_security_group" "autopilot_sg" {
+  name = "autopilot-sg"
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -37,15 +37,23 @@ resource "aws_security_group" "allow_ssh" {
   }
 }
 
-## EC2 Instance
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3a.medium"
-  subnet_id     = aws_subnet.example_subnet.id
-  security_groups = [aws_security_group.allow_ssh.name]
+resource "aws_instance" "app_server" {
+  ami                    = "ami-0dee22c13ea7a9a67"
+  instance_type         = "t3a.medium"
+  iam_instance_profile   = "ssm_profile"
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+  user_data = <<-EOF
+                #!/bin/bash
+                docker setup script here
+                EOF
+  tags = {
+    Name = "Autopilot-Backend"
+  }
 }
 
-## Elastic IP
-resource "aws_eip" "example_eip" {
-  instance = aws_instance.example_instance.id
+resource "aws_eip" "lb" {
+  instance = aws_instance.app_server.id
 }
