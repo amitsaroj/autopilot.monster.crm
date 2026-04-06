c8053ebce03e38f44e21184001dea10edca0f277
Amit Saroj <sarojamit4956@gmail.com>
2026-04-07 00:45:05 +0530

    Updated main.tf to remove provider block and restored original resources with instance_type changed to t3a.medium.

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
-# Terraform Configuration 
+# Terraform Configuration
 
-provider "aws" {
-  region = "ap-south-1"
-}
+# Provider configuration removed as per request.
 
-resource "aws_instance" "app" {
-  ami           = "ami-123456"
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe01e"
   instance_type = "t3a.medium"
-  # ... other configurations
+  ... # other configurations remain unchanged
 }
