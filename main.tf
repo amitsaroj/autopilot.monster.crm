5e7d2e269fb7994e4ad206194069d48228f179dc
Amit Saroj <sarojamit4956@gmail.com>
2026-04-07 00:46:32 +0530

    Updated main.tf with complete Terraform resources.

--- main.tf
+++ main.tf
@@ -1,9 +1,51 @@
-# Terraform Configuration
+# Terraform Configuration for AWS
 
-# Provider configuration removed as per request.
+## IAM Roles
+resource "aws_iam_role" "example_role" {
+  name               = "example_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Effect   = "Allow"
+      Sid      = ""
+    }]
+  })
+}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe01e"
+## Security Groups
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH from anywhere"
+  vpc_id      = aws_vpc.example_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+}
+
+## EC2 Instance
+resource "aws_instance" "example_instance" {
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t3a.medium"
-  ... # other configurations remain unchanged
+  subnet_id     = aws_subnet.example_subnet.id
+  security_groups = [aws_security_group.allow_ssh.name]
+}
+
+## Elastic IP
+resource "aws_eip" "example_eip" {
+  instance = aws_instance.example_instance.id
 }
